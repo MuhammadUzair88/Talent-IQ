bbbe52749327123aaff125088d9e4bfcd18ae880
Your Name <uzairthreestar1456@gmail.com>
2025-12-02 18:45:22 +0500

    deploy

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,31 +1,20 @@
 
 import express from "express";
 import dotenv from "dotenv";
-import Path from "path";
+
 
 dotenv.config();
 
 const app=express();
 
-const __dirname=Path.resolve()
 
-app.get("/health",(req,res)=>{
+app.get("/",(req,res)=>{
     res.status(200).json({msg:"Server is running"})
 });
 
 app.get("/books",(req,res)=>{
     res.status(200).json({msg:"this is the books endpoint"})
 });
-
-//make our app ready for deployment
-
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(Path.join(__dirname,"../Frontend/dist")))
-
-    app.get("/{*any}",(req,res)=>{
-       res.sendFile(Path.join(__dirname,"../Frontend","dist","index.html"));   
-    })
-}
 
 
 app.listen(process.env.PORT,()=>{
