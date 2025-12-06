a81b09027bfe8371054b15306780191d7e137128
Your Name <uzairthreestar1456@gmail.com>
2025-12-06 15:50:16 +0500

    stream user setup

--- Backend/src/lib/inngest.js
+++ Backend/src/lib/inngest.js
@@ -1,6 +1,7 @@
 import {Inngest} from 'inngest';
 import { connectDB } from './DB.js';
 import User from '../models/User.js';
+import { createStreamUser, deleteStreamUser } from './stream.js';
 
 
 export const inngest = new Inngest({ id: "talent-iq" });
@@ -18,6 +19,11 @@ const syncUser=inngest.createFunction(
             profileImage:image_url,
         }
         await User.create(newUser);
+        await createStreamUser({
+            id:newUser.clerkId,
+            name:newUser.name,
+            image:newUser.profileImage,
+        });
     } 
 )
 
@@ -28,6 +34,7 @@ const deleteUserFromDB=inngest.createFunction(
         await connectDB();
         const {id}=event.data;
         await User.deleteOne({clerkId:id});
+        await deleteStreamUser(id.toString());
     } 
 );
 
