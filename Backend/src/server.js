15f6a72f57200826dec5bb034ac1e78f146c793e
Your Name <uzairthreestar1456@gmail.com>
2025-12-06 13:17:07 +0500

    hello

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,16 +2,23 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/DB.js";
+import cors from "cors"
+import {serve} from "inngest/express";
+import { inngest,functions } from "./lib/inngest.js";
 
 
 dotenv.config();
 
 const app=express();
 
+app.use(express.json())
+app.use(cors())
 
 app.get("/",(req,res)=>{
     res.status(200).json({msg:"Server is running"})
 });
+
+app.use('/api/inngest',serve({client:inngest,functions}))
 
 app.get("/books",(req,res)=>{
     res.status(200).json({msg:"this is the books endpoint"})
